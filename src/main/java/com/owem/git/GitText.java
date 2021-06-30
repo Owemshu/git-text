04d0a19e28349b71daa904b2616ddb6db549542a
Owem <shuhenxin@qq.com>
2021-06-30 17:02:28 +0800

    master text

--- src/main/java/com/owem/git/GitText.java
+++ src/main/java/com/owem/git/GitText.java
@@ -7,5 +7,6 @@ public class GitText {
         System.out.println("Hello Git3");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("Push text");
     }
 }
