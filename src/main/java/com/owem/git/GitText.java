2fc0d18eb7ab9d5b3b604e14be3ed24c9456ffd6
Owem <shuhenxin@qq.com>
2021-06-30 16:43:03 +0800

    second commit

--- src/main/java/com/owem/git/GitText.java
+++ src/main/java/com/owem/git/GitText.java
@@ -3,5 +3,6 @@ package com.owem.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
